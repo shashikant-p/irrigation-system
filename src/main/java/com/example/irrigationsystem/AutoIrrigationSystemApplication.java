0111bf520220dcda03cf03e1a1e3ecf77a3019f9
shashikant <shashikant.p@gmail.com>
2022-11-22 16:51:13 +0530

    Added Swagger Suppport

--- src/main/java/com/example/irrigationsystem/AutoIrrigationSystemApplication.java
+++ src/main/java/com/example/irrigationsystem/AutoIrrigationSystemApplication.java
@@ -2,6 +2,7 @@ package com.example.irrigationsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
