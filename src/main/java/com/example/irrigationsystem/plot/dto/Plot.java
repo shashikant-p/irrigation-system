535ad56312c6c04a08917b2c25160405d5e60ede
shashikant <shashikant.p@gmail.com>
2022-11-23 08:28:57 +0530

    Added Documentation

--- src/main/java/com/example/irrigationsystem/plot/dto/Plot.java
+++ src/main/java/com/example/irrigationsystem/plot/dto/Plot.java
@@ -8,6 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.AccessMode;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,22 +24,28 @@ import lombok.ToString;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "The Plot Object")
 public class Plot implements Serializable {
 
+	@Schema(description = "Unique identifier for a plot", accessMode = AccessMode.READ_ONLY)
 	private String plotId;
 
-	@NotEmpty
+	@Schema(description = "The plot's address", requiredMode = RequiredMode.REQUIRED)
 	private String address;
 
+	@Schema(description = "The plot's city", requiredMode = RequiredMode.REQUIRED)
 	@NotEmpty
 	private String city;
 
+	@Schema(description = "The plot's province/state", requiredMode = RequiredMode.REQUIRED)
 	@NotEmpty
 	private String province;
 
+	@Schema(description = "The area of covered by the plot in square feet", requiredMode = RequiredMode.REQUIRED)
 	@NotEmpty
 	private String area;
 
+	@Schema(description = "The plot's configurations", accessMode = AccessMode.READ_ONLY)
 	@JsonInclude(Include.NON_NULL)
 	private List<PlotConfig> configs;
 }
--- src/test/java/com/example/irrigationsystem/plot/controller/PlotControllerTest.java
+++ src/test/java/com/example/irrigationsystem/plot/controller/PlotControllerTest.java
@@ -29,24 +29,22 @@ public class PlotControllerTest {
 	PlotService plotService;
 
 	static Plot testPlot;
-	
+
 	static List<Plot> plots;
-	
+
 	@BeforeAll
 	static void setup() {
-		testPlot = Plot.builder()
-				.address("Test Address").area("40")
-				.city("Test City").plotId("1234")
+		testPlot = Plot.builder().address("Test Address").area("40").city("Test City").plotId("1234")
 				.province("Test Province").build();
 		plots = new ArrayList<Plot>();
 		plots.add(testPlot);
 	}
-	
+
 	@Test
 	public void testFindAll() {
 
 		when(plotService.getPlots()).thenReturn(plots);
-		
+
 		ResponseEntity<ResponseObject> response = plotController.getPlots();
 
 		assertThat(response.getStatusCodeValue()).isEqualTo(200);
@@ -54,12 +52,12 @@ public class PlotControllerTest {
 		assertThat(((List) response.getBody().getData()).size()).isEqualTo(1);
 		assertThat(((Plot) ((List) response.getBody().getData()).get(0)).getPlotId()).isEqualTo("1234");
 	}
-	
+
 	@Test
 	public void testFindByPlotId() {
 
 		when(plotService.getPlot("1234")).thenReturn(testPlot);
-		
+
 		ResponseEntity<ResponseObject> response = plotController.getPlot("1234");
 
 		assertThat(response.getStatusCodeValue()).isEqualTo(200);
